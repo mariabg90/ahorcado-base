40b5cf54e97ff6a3b08ed16908e49718585d01b6
Tony G. Bolaño <tony.gonzalez@tyris-software.com>
2025-03-27 19:05:41 +0100

    feat: ejemplo de uso

--- hangman/utils.py
+++ hangman/utils.py
@@ -1,22 +1,70 @@
 import pygame as pg
 
-from .constants import BLUE, GRAPHICS, GREY, LINE_WIDTH
+from .constants import BLUE, GRAPHICS, GREEN, GREY, LINE_WIDTH, RED, WHITE, WINDOW_H, WINDOW_W
 
 
-def draw_base(screen):
+def draw_base(screen, margin_x=10, margin_y=50):
+    """Dibuja el ahorcado completo deactivado."""
     for key in GRAPHICS:
-        draw_part(screen, key)
+        draw_part(screen, key, False, margin_x, margin_y)
 
 
-def draw_part(screen, key, active=False):
+def offset_point(point, margin_x, margin_y):
+    return (point[0] + margin_x, point[1] + margin_y)
+
+
+def draw_part(screen, key, active=False, margin_x=10, margin_y=50):
     color = GREY
     if active:
         color = BLUE
     if key == 'head':
-        center = GRAPHICS.get(key)[0]
+        center = offset_point(GRAPHICS.get(key)[0], margin_x, margin_y)
         radius = GRAPHICS.get(key)[1]
         pg.draw.circle(screen, color, center, radius, LINE_WIDTH)
     else:
-        start = GRAPHICS.get(key)[0]
-        end = GRAPHICS.get(key)[1]
+        start = offset_point(GRAPHICS.get(key)[0], margin_x, margin_y)
+        end = offset_point(GRAPHICS.get(key)[1], margin_x, margin_y)
         pg.draw.line(screen, color, start, end, LINE_WIDTH)
+
+
+def draw_word(screen, font, word):
+    word_surface = font.render(word, True, WHITE)
+    word_width, word_height = word_surface.get_size()
+    screen.blit(word_surface, ((WINDOW_W-word_width) //
+                2, WINDOW_H-word_height*1.5))
+
+
+def draw_letters(screen, font: pg.font.SysFont, x, y, fail='', success=''):
+    a = ord('A')
+    z = ord('Z')
+    n = ord('N')
+    nn = ord('Ñ')
+    char_width = 45
+    line_height = 55
+    pos_x = x
+    pos_y = y
+    for ascii in range(a, z+1):
+        color = GREY
+        if chr(ascii) in fail.upper():
+            color = RED
+        elif chr(ascii) in success.upper():
+            color = GREEN
+        pos_x, pos_y = draw_letter(
+            screen, font, x, char_width, line_height, pos_x, pos_y, ascii, color)
+
+        if ascii == n:
+            pos_x, pos_y = draw_letter(
+                screen, font, x, char_width, line_height, pos_x, pos_y, nn, color)
+
+
+def draw_letter(screen, font, x, char_width, line_height, pos_x, pos_y, ascii, color):
+    letter = font.render(chr(ascii), True, color)
+    w, h = letter.get_size()
+    top = pos_y + line_height - h
+    left = pos_x + (char_width - w) // 2
+    screen.blit(letter, (left, top))
+    pos_x = pos_x + char_width
+    if pos_x >= WINDOW_W - char_width:
+        pos_y = pos_y + line_height
+        pos_x = x
+    return pos_x, pos_y
--- sample.py
+++ sample.py
@@ -1,10 +1,14 @@
 import pygame as pg
 from hangman import utils
-from hangman.constants import BLUE, GRAPHICS, GREY, LINE_WIDTH
+from hangman.constants import BLACK, BLUE, GRAPHICS, GREY, LINE_WIDTH, WHITE, WINDOW_H, WINDOW_W
 
-display = pg.display.set_mode((500, 500), 0, 32)
+pg.init()
+display = pg.display.set_mode((WINDOW_W, WINDOW_H), 0, 32)
 pg.display.set_caption('Juego del ahorcado')
 clock = pg.time.Clock()
+font_name = pg.font.get_default_font()
+font = pg.font.SysFont(font_name, 70)
+letters_font = pg.font.SysFont(font_name, 60)
 
 playing = True
 
@@ -17,12 +21,16 @@ while playing:
         if event.type == pg.QUIT:
             playing = False
 
-    utils.draw_base(display)
+    utils.draw_base(display, 30)
 
     if counter % 20 == 0:
         status += 1
 
     for index, key in enumerate(GRAPHICS):
         if index <= status:
-            utils.draw_part(display, key, True)
+            utils.draw_part(display, key, True, 30)
+
+    utils.draw_word(display, font, '-ee---d---')
+    utils.draw_letters(display, letters_font, 360, 50, 'aeiou', 'bc')
+
     pg.display.flip()
